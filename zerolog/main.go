bb77390d5682cb1c22bb6f39a69924b4dd83b76d
Shubham Prajapati <89298156+p-shubh@users.noreply.github.com>
2024-08-10 22:47:51 +0000

    adding zerolog

--- zerolog/main.go
+++ zerolog/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"log"
+	"github.com/rs/zerolog/log"
 
 	"github.com/rs/zerolog"
 )
 
 func main() {
+	Log2()
+}
+
+func Log1() {
 	// UNIX Time is faster and smaller than most timestamps
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
 	log.Print("hello world")
+}
+
+func Log2() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	log.Debug().
+		Str("Scale", "833 cents").
+		Float64("Interval", 833.09).
+		Msg("Fibonacci is everywhere")
+
+	log.Debug().
+		Str("Name", "Tom").
+		Send()
 }
