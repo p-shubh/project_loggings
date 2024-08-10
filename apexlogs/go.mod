8a61ee7135ac98988c14cdf6163ef614fcfac04d
Shubham Prajapati <89298156+p-shubh@users.noreply.github.com>
2024-08-10 23:09:16 +0000

    adding 	"github.com/apex/log"

---
+++ apexlogs/go.mod
@@ -0,0 +1,7 @@
+module apexlogs
+
+go 1.22.5
+
+require github.com/apex/log v1.9.0
+
+require github.com/pkg/errors v0.8.1 // indirect
---
+++ apexlogs/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/apex/log"
+)
+
+func main() {
+	ctx := log.WithFields(log.Fields{
+		"file": "something.png",
+		"type": "image/png",
+		"user": "tobi",
+	})
+
+	for range time.Tick(time.Millisecond * 500) {
+		ctx.Info("upload")
+		ctx.Info("upload complete")
+		ctx.Warn("upload retry")
+		ctx.WithError(errors.New("unauthorized")).Error("upload failed")
+		ctx.Errorf("failed to upload %s", "img.png")
+		fmt.Println()
+	}
+}
